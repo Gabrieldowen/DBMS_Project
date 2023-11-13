aea433ae352e36733067d565993f1f5ceb7eccf3
Gabriel <Gabriel.d.owen-1@ou.edu>
2023-11-12 20:32:37 -0600

    Added Account creation for process accounts

--- usp_CreateAccount.sql
+++ usp_CreateAccount.sql
@@ -3,9 +3,14 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 -- Create a stored procedure to insert a new process with type information
--- usp_CreateNewDepartment 1, 'Datatatatatatatta'
--- InsertProcessWithType 3, 1, 1, 2, 'THE FIT'
-CREATE PROCEDURE [dbo].[usp_CreateAccount]
+/*
+EXEC usp_CreateNewDepartment 1, 'Datatatatatatatta'
+EXEC usp_CreateProcess 3, 1, 1, 2, 'THE FIT'
+EXEC [dbo].[usp_CreateAccount] 3, 1, '2023-11-12', 1, 3
+*/
+-- 
+-- 
+ALTER PROCEDURE [dbo].[usp_CreateAccount]
 (
     @ProcessID          INT,
     @AccountNumber      INT,
@@ -23,7 +28,12 @@ BEGIN
     BEGIN
         IF NOT EXISTS (SELECT 1 FROM Account WHERE AccountNumber = @AccountNumber)
         BEGIN
-            PRINT 'Process with type information inserted successfully.';
+            INSERT INTO Account(AccountNumber, DateCreated, Category, AccountTypeID)
+            VALUES (@AccountNumber, @DateCreated, @Category, @AccountTypeID)
+
+            UPDATE Process
+            SET AccountNumber = @AccountNumber
+            WHERE ProcessID = @ProcessID
         END
     END
 END;
